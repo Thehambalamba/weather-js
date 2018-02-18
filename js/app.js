c6fd0861411fcf32cbc1d592342e03bcb3a46ee5
Nikola Pervic <nikolapervic@me.com>
2018-02-19 00:30:22 +0100

    Data in UI
    Created the UI class and added the UI elements as variables. Created a paint class to add data to the UI.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 const weather = new Weather('Boston', 'MA');
+const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', getWeather)
 
 function getWeather() {
     weather.getWeather()
         .then((results) => {
-            console.log(results);
+            // console.log(results);
+            ui.paint(results);
         })
         .catch((err) => console.log(err));
 }
